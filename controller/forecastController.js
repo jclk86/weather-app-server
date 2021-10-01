6c3d798dfaa683034d73f47764ef18aa2aa35038
John Chan <johnchancontact@gmail.com>
2021-10-01 13:32:27 -0400

    FIX: changed file structure

--- controller/forecastController.js
+++ controller/forecastController.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const getForecast = async (req, res) => {
   try {
-    const fetch_response = await axios.get(`${process.env.OPEN_WEATHER}`);
+    const fetch_response = await axios.get(`${process.env.OPEN_WEATHER_BASE_URL}forecast?zip${process.env.}&appid=${}`);
   } catch (err) {
 
   }
