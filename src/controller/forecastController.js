4cb2a3edd1a8019296781694ef0899112c2af070
John Chan <johnchancontact@gmail.com>
2021-10-03 12:18:54 -0400

    FEAT: added datareturn from api

--- src/controller/forecastController.js
+++ src/controller/forecastController.js
@@ -1,12 +1,13 @@
 const axios = require("axios");
 const settings = require("../settings");
-const { checkIfNums } = require("./utilities");
+const { checkIfNums, findCoordinates } = require("./utilities");
 const { postcodeValidator } = require("postcode-validator");
 
 const getForecast = async (req, res, next) => {
   const { zipCode } = req.params;
 
   // Only supports US for now
+  // TODO: find international support
   const isValidZipCode = await postcodeValidator(`${zipCode}`, "US");
 
   if (
@@ -19,16 +20,23 @@ const getForecast = async (req, res, next) => {
 
   // Fetches 5-day forecast
   const fetchForecast = axios.get(
-    `${settings.weather.forecast_url}zip=${zipCode}&appid=${settings.weather.api_key}&cnt=5`
+    `${settings.weather.forecast_url}zip=${zipCode}&appid=${settings.weather.api_key}`
   );
 
-  // Fetches latitude and longitude from zip code
+  // Fetches latitude and longitude from zip code - use google api for beyond US
   const fetchGeocode = axios.get(
     `${settings.google.geocode_url}address=${zipCode}&key=${settings.google.api_key}`
   );
 
   let [geocode, forecast] = await Promise.all([fetchGeocode, fetchForecast]);
 
+  // Using JSON asset
+  // const coordinates = await findCoordinates(zipCode);
+  // const uvIndexResponse = await axios.get(
+  //   `${settings.weather.uvIndex_url}lat=${coordinates[0]}&lon=${coordinates[1]}&appid=${settings.weather.api_key}`
+  // );
+
+  // Using google api coordinate results
   // Fetches uv index for current day using geocode data
   const uvIndexResponse = await axios.get(
     `${settings.weather.uvIndex_url}lat=${geocode.data.results[0].geometry.location.lat}&lon=${geocode.data.results[0].geometry.location.lng}&appid=${settings.weather.api_key}`
@@ -36,22 +44,31 @@ const getForecast = async (req, res, next) => {
 
   // Forecast result array
   const fiveDayForecast = forecast.data.list.map(
-    ({ dt, main: { temp, feels_like, humidity }, weather: { icon } }) => {
+    ({
+      dt_txt,
+      main: { temp, temp_min, temp_max, feels_like, humidity },
+      weather,
+      wind,
+    }) => {
       return {
-        date: dt,
+        date: dt_txt,
         main: {
           temp: temp,
+          temp_max: temp_max,
+          temp_min: temp_min,
           feel: feels_like,
           humidity: humidity,
+          wind: wind,
         },
-        icon: icon,
+        icon: weather[0].icon,
+        description: weather[0].main,
       };
     }
   );
 
   res.status(200).send({
-    forecast: fiveDayForecast,
-    UV_INDEX: uvIndexResponse.data.current.uvi,
+    data: fiveDayForecast,
+    uv_index: uvIndexResponse.data.current.uvi,
   });
 };
 
--- src/controller/utilities.js
+++ src/controller/utilities.js
@@ -1,3 +1,9 @@
+const coordinates = require("../assets/zip-codes-to-geo-coords.json");
+
 exports.checkIfNums = (str) => {
   return /^\d+$/.test(str);
+};
+
+exports.findCoordinates = (zipcode) => {
+  return coordinates[zipcode];
 };
